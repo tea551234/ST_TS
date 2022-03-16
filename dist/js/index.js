1c21214e830bf02d5715f3656ffe317b4f10e328
tea551234 <tea551234@gmail.com>
2022-03-16 11:01:29 +0800

    2022 0316 1101

---
+++ dist/js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+let st_TypeScript = 'HI ';
+console.log(st_TypeScript);
+// --------------------- enum ------------------
+var LiveStatus;
+(function (LiveStatus) {
+    LiveStatus[LiveStatus["success"] = 0] = "success";
+    LiveStatus[LiveStatus["FAILED"] = -1] = "FAILED";
+    LiveStatus[LiveStatus["STREAMING"] = 1] = "STREAMING";
+})(LiveStatus || (LiveStatus = {}));
+const STATUS = LiveStatus;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvVFMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLElBQUksYUFBYSxHQUFVLEtBQUssQ0FBQTtBQUVoQyxPQUFPLENBQUMsR0FBRyxDQUFDLGFBQWEsQ0FBQyxDQUFDO0FBRzNCLGdEQUFnRDtBQUdoRCxJQUFLLFVBSUo7QUFKRCxXQUFLLFVBQVU7SUFDWCxpREFBVyxDQUFBO0lBQ1gsZ0RBQVcsQ0FBQTtJQUNYLHFEQUFhLENBQUE7QUFDakIsQ0FBQyxFQUpJLFVBQVUsS0FBVixVQUFVLFFBSWQ7QUFDRCxNQUFNLE1BQU0sR0FBRyxVQUFVLENBQUEifQ==
---
+++ src/TS/index.ts
@@ -0,0 +1,14 @@
+let st_TypeScript:string = 'HI '
+
+console.log(st_TypeScript);
+
+
+// --------------------- enum ------------------
+
+
+enum LiveStatus{
+    success = 0,
+    FAILED = -1,
+    STREAMING = 1
+}
+const STATUS = LiveStatus
